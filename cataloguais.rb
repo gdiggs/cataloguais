f6285f502e38f54eef95181f517fd46f7261cbb3
GordonDiggs <gordon@gordondiggs.com>
2011-09-11 18:37:42 -0400

    configure production db

--- cataloguais.rb
+++ cataloguais.rb
@@ -2,13 +2,22 @@
 # encoding: utf-8
 require 'rubygems'
 require 'bundler'
+require 'uri'
 
 Bundler.require
 require "sinatra/config_file"
 
+configure :production do
+  # next 4 lines from http://bit.ly/nyu6SD
+  uri =  URI.parse(ENV['MONGOHQ_URL'])
+  @mongo_connection = Mongo::Connection.from_uri(uri)
+  @mongo_db = @mongo_connection.db(uri.path.gsub(/^\//, ''))
+  @mongo_db.authenticate(uri.user, uri.password)
+end
+
 configure do
   config_file 'settings.yml'
-  MongoMapper.database = ENV['MONGOHQ_URL'] || settings.database
+  MongoMapper.database = settings.database
 end
 
 get '/' do
