136ba1d552210d4be12087a9d93e4793749cf589
GordonDiggs <gordon@gordondiggs.com>
2011-09-17 16:20:32 -0400

    Only clear text inputs, not the submit button when adding a new item

--- public/scripts.js
+++ public/scripts.js
@@ -18,7 +18,7 @@ $(document).ready(function() {
       success: function(response) {
         showMessage(response.message);
         $(response.item_markup).appendTo('table');
-        $form.find('input').val('');
+        $form.find('input[type=text]').val('');
       }
     });
 
